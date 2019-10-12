8dba2d8d42e105596ffc8432385161c971a74504
Brayden Copley <braydenc303@gmail.com>
2019-10-11 20:08:12 -0600

    Switch cases working

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -17,11 +17,50 @@ public class Main {
             case 1:
             System.out.println("Value was 1");
             break;
+
             case 2:
             System.out.println("Value was 2");
             break;
+
+            case 3:
+            case 4:
+            case 5:
+            System.out.println("Value was 3, 4, or 5");
+            break;
+
             default:
-            System.out.println("Value was not 1 or 2");
+            System.out.println("Value was not 1-5");
+        }
+
+        // Create a new switch statement using char instead of int
+        // create a new char variable
+        // create a switch statement testing for 
+        // A, B, C, D or E
+        // display a message if any of these are found and the break
+        // Add a default which displays a message saying not found.
+
+        char letter = 'A';
+
+        switch(letter) {
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'E':
+            System.out.println(letter + " was found");
+            break;
+            default:
+            System.out.println("Not found");
+        }
+
+        String month = "JanUary";
+
+        switch(month.toLowerCase()) {
+            case "january":
+            System.out.println("Jan");
+            break;
+            default:
+            System.out.println("Not Jan");
         }
 
     }

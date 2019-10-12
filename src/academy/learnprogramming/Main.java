7f67ef5661a354bc6cfd8a96c61e762394c4ec6b
Brayden Copley <braydenc303@gmail.com>
2019-10-11 20:23:09 -0600

    Day challenge switch statement working

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -63,5 +63,13 @@ public class Main {
             System.out.println("Not Jan");
         }
 
+        DayOfTheWeek.printDayOfTheWeek(0);
+        DayOfTheWeek.printDayOfTheWeek(1);
+        DayOfTheWeek.printDayOfTheWeek(2);
+        DayOfTheWeek.printDayOfTheWeek(3);
+        DayOfTheWeek.printDayOfTheWeek(4);
+        DayOfTheWeek.printDayOfTheWeek(5);
+        DayOfTheWeek.printDayOfTheWeek(6);
+        DayOfTheWeek.printDayOfTheWeek(7);
     }
 }
